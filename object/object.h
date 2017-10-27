d49f5e50ff2f5b1cc53371f7a461fec5194f67c9
KouKeioh <kou2013kou.kou@gmail.com>
2017-10-28 08:10:19 +0900

    virtualいらなくね？ってことで削除

--- object/object.h
+++ object/object.h
@@ -1,5 +1,6 @@
-#pragma once
+//オブジェクトを作る際の基本となるクラス
 
+#pragma once
 #include "header\define.h"
 #include "header\impact.h"
 
@@ -12,7 +13,7 @@ public:
 	float graphics_position_x, graphics_position_y;//画像ポジション
 
 	void setup(float pos_x, float pos_y, char* path, float radius = -1);//初期化処理(xの初期位置,yの初期位置,画像への相対パス,円の場合は半径を入力)
-	virtual void update(float moved_x, float moved_y, float rate = 1.0f);//アニメーション処理。(iフレームの移動量をmoved_x,moved_yに書くrateは移動倍率1.0fで一倍)
+	void update(float moved_x, float moved_y, float rate = 1.0f);//アニメーション処理。(1フレームの移動量をmoved_x,moved_yに書くrateは移動倍率1.0fで一倍)
 	void draw(bool wire = false);//描写
 
 	void mouseMovedUpdate(int x, int y);//ofApp::mouseMoved(int x,int y)の中に記述する。マウスを動かしたときにヒット判定をさせる処理。
